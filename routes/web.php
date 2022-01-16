6d36c64951e34cc2cfa532b3670611c0aa1210e9
JEBASS <JEBASBS@GMAIL.COM>
2022-01-16 15:05:45 -0300

    cadrasto de usuario

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get("alcateia",[\App\Http\Controllers\UserController::class,"getAlcateia"]);
