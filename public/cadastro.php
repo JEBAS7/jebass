a92cb232eebfe0594e66ac4ca86583a5be3ec05c
JEBASS <JEBASBS@GMAIL.COM>
2022-01-19 04:56:55 -0300

    cadastrando

---
+++ public/cadastro.php
@@ -0,0 +1,40 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$connect = mysqli_connect('localhost', 'root', '', 'alcateia') or die('Não foi possível conectar');
+$db = mysqli_select_db($connect,'alcateia');
+$query_select = "SELECT nome FROM membros WHERE nome = '$nome'";
+$select = mysqli_query($connect, $query_select);
+$array = mysqli_fetch_array($select);
+$logarray = $array['nome'];
+
+  if($nome == "" || $nome == null){
+    echo"<script language='javascript' type='text/javascript'>
+    alert('O campo nome deve ser preenchido');window.location.href='
+    cadastro.html';</script>";
+
+    }else{
+      if($logarray == $nome){
+
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse nome já existe');window.location.href='
+        cadastro.html';</script>";
+        die();
+
+      }else{
+        $query = "INSERT INTO membros (nome,email) VALUES ('$nome','$email')";
+        $insert = mysqli_query($connect, $query);
+
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='nome.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+?>
